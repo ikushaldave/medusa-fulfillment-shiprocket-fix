8beb0ca8abcef12c3942ba7938074ec25412b815
Kushal Dave <ikushaldave@gmail.com>
2025-05-28 18:20:19 +0530

    fix: identifier name

--- src/services/shiprocket-fulfillment.js
+++ src/services/shiprocket-fulfillment.js
@@ -11,7 +11,7 @@ import {
 import { Lifetime } from "awilix";
 
 class ShiprocketFulfillmentService extends AbstractFulfillmentService {
-  static identifier = "fp_shiprocket";
+  static identifier = "shiprocket";
   static LIFE_TIME = Lifetime.TRANSIENT;
 
   constructor(
